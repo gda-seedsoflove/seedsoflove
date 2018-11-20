2cd9c0d10cac5e8b6a7cda311115cd27c12a55b4
Kenny Doan <kdoan5@ucsc.edu>
2018-11-20 15:13:11 -0800

    Draft Detector
    Added detector script and prefabs

--- Seeds of Love/Assets/Script/Detector.cs
+++ Seeds of Love/Assets/Script/Detector.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Detector : MonoBehaviour {
 
+    bool touching = false;
+    float time;
+    GameObject thing;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,23 @@ public class Detector : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (touching)
+        {
+            time += Time.deltaTime;
+
+        }
+        if(time >= .5)
+        {
+            Destroy(thing);
+            touching = false;
+        }
 	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        touching = true;
+        thing = collision.gameObject;
+        
+    }
 }

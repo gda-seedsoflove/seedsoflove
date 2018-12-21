117d86476e2a594c0a36f890919cb5a2717ea383
Kenny Doan <kdoan5@ucsc.edu>
2018-12-20 18:46:33 -0800

    Test

---
+++ Seeds of Love/Assets/Tests/Interactive/NoteTimingTest/NoteGenerator.cs
@@ -0,0 +1,42 @@
+﻿using Script.Song;
+using UnityEngine;
+
+namespace Tests.Interactive.NoteTimingTest
+{
+    public class NoteGenerator : NoteManager
+    {
+        public float SpawnTimeInterval;
+
+        private float _nextNoteSpawnTime;
+
+        public override float CurrentSongTime
+        {
+            get { return Time.time; }
+        }
+
+        protected override float[] LanePositions
+        {
+            get { return new[] {-6.2f, -5.4f, -4.6f, -3.8f}; }
+        }
+
+        private void Start()
+        {
+            _nextNoteSpawnTime = Time.time;
+        }
+
+        private void Update()
+        {
+            // If the next note to be spawned is overdue, spawn it, and queue
+            // the time for the next note
+            if (_nextNoteSpawnTime <= CurrentSongTime)
+            {
+                CreateNote(
+                    _nextNoteSpawnTime + DisplayedTimeBefore,
+                    Random.Range(0, LanePositions.Length)
+                );
+
+                _nextNoteSpawnTime = CurrentSongTime + SpawnTimeInterval;
+            }
+        }
+    }
+}

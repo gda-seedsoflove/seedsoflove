3ef780c72e219f16297f12afface42b1813ba9ae
Kenny Doan <kdoan5@ucsc.edu>
2018-12-20 20:38:45 -0800

    Sheep_Burst

---
+++ Seeds of Love/Assets/Script/Behaviour/ParticleScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour {
+
+    private ParticleSystem ps;
+    private bool played;
+    public float delay;
+	// Use this for initialization
+	void Start () {
+        ps = gameObject.GetComponent<ParticleSystem>();
+        played = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (delay <= 0 && played == false)
+        {
+            ps.Play();
+            played = true;
+        }
+        else
+        {
+            delay -= Time.deltaTime;
+        }
+
+        if(ps.isPlaying == false && played == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

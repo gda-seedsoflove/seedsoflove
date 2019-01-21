30af1fda573126667e89cf799aa36189fc140c7b
Thomas Applewhite <thomashapplewhite@gmail.com>
2019-01-20 17:00:03 -0800

    Created a unique prefab that handles note hit SFX
    >Audio Source removed from Carrot_Burst_Effect
    >new prefab Note_Hit_SFX in Rythem Prefabs. Has 1 SFX audio source and uses a SoundEffectScript that is almost identical to the ParticalEffectScript
    >Changed EffectsManager to create a Note_Hit_SFX at the same time and place as a Carrot_Burst_Effect

--- Seeds of Love/Assets/Script/Behaviour/EffectsManager.cs
+++ Seeds of Love/Assets/Script/Behaviour/EffectsManager.cs
@@ -9,6 +9,8 @@ public class EffectsManager : MonoBehaviour
 {
     // The Particle Prefab that will be created when the Note is hit
     public GameObject ParticleEffect;
+    //The Sound Prefab that will be created when the Note is hit
+    public GameObject SoundEffect;
 
     private void Start()
     {
@@ -17,14 +19,16 @@ public class EffectsManager : MonoBehaviour
     }
 
     /*
-     * Creates the ParticleEffect prefab on the note and stops the note from moving
+     * Creates the ParticleEffect and Note_Hit_SFX prefab on the note and stops the note from moving
      * Animates the Note for the Note hit
      */
     public void PlayEffect()
     {
         GameObject instance = (GameObject)Instantiate(ParticleEffect, transform.position, Quaternion.identity);
+        GameObject instanceSound = (GameObject)Instantiate(SoundEffect, transform.position, Quaternion.identity);
         Destroy(instance, 1f);
-        if(GetComponent<NoteMovement>())
+        Destroy(instanceSound, 1f);
+        if (GetComponent<NoteMovement>())
         {
             GetComponent<NoteMovement>().enabled = false;
         }

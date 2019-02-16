1f8f5125a4e703ae50514eb0973c6aa9e6ed2947
Thomas Applewhite <thomashapplewhite@gmail.com>
2019-02-15 19:49:59 -0800

    Began new system foundation and created AudioObject
    Created a generic AudioObject that can take delay, volume, and audio clips as arguments and play sounds at those volumes and delays without setting anything specific in the prefab.

---
+++ Seeds of Love/Assets/Script/Audio/SoundPlayer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour {
+
+    //ATTENTION: DO NOT SET THE AUDIOCLIP FIELD OF THIS OBJECT
+    //I don't know what it will do, but it migh cause everything to explode
+
+    AudioSource SoundSource;    //The object's AudioSource component
+    float delay;    //How much delay before the sound plays
+    float volume;   //The AudioSource component's volume setting
+    bool soundStarted;  //Whether or not the audio has started
+
+    //This function is called in SoundControlScript and sets the properties of the AudioObject
+    void PlaySound(float delayInput, float volumeInput, AudioClip clipInput)
+    {
+        SoundSource.clip = clipInput;       //Sets the AudioSource's clip (set to 'none' by default).
+        SoundSource.volume = volumeInput;   //Sets the AudioSource's volume (set to 1.0 by default).
+        delay = delayInput;                 //Sets the argument value for PlayDelayed
+        SoundSource.PlayDelayed(delay);                 //Plays the audio after the argument's value
+        soundStarted = true;                //The sound is now playing
+    }
+
+	// Use this for initialization
+	void Start () {
+        soundStarted = false;
+        SoundSource = gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (soundStarted && !SoundSource.isPlaying)
+        {
+            Destroy(gameObject);            //Object self destructs once the sound is done playing.
+        }
+	}
+
+}

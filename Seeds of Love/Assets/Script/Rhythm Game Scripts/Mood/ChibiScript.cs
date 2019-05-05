cef22dec87a645a12635e1c2ac4b4b4b036c9677
Kenny Doan <kdoan5@ucsc.edu>
2019-05-05 09:45:41 -0700

    Chibis Now briefly flash sad
    When th eplayer misses a note, the chibis flash sad for brief moment.

--- Seeds of Love/Assets/Script/Rhythm Game Scripts/Mood/ChibiScript.cs
+++ Seeds of Love/Assets/Script/Rhythm Game Scripts/Mood/ChibiScript.cs
@@ -10,17 +10,25 @@ public class ChibiScript : MonoBehaviour {
     public Sprite HappyChibi;
     public Sprite NormalChibi;
     public Sprite SadChibi;
+
+    private float PrevMood;
+    private float SadTime;
+
 	// Use this for initialization
 	void Start () {
         //Debug.Log(BeatmapReader.GetComponent<BeatmapReader>().bpm/60);
         GetComponent<Animator>().speed = BeatmapReader.GetComponent<BeatmapReader>().bpm/60;
+        if (PlayerData.instance != null)
+        {
+            PrevMood = PlayerData.instance.Mood;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (PlayerData.instance != null && BeatmapReader.GetComponent<BeatmapReader>().songEnd == false)
         {
-            if (PlayerData.instance.Mood < .5f)
+            if (PlayerData.instance.Mood < .5f || SadTime >= 0)
             {
                 GetComponent<SpriteRenderer>().sprite = SadChibi;
             }
@@ -32,6 +40,16 @@ public class ChibiScript : MonoBehaviour {
             {
                 GetComponent<SpriteRenderer>().sprite = NormalChibi;
             }
+
+            if (PlayerData.instance.Mood < PrevMood)
+            {
+                SadTime += .35f; // Sad time will increase but .1 seconds
+            }
+            else if(SadTime >= 0)
+            {
+                SadTime -= Time.deltaTime;
+            }
+            PrevMood = PlayerData.instance.Mood;
         }
         else
         {

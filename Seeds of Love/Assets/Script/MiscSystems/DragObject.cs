5ccb903f4a700d60a38b444b728a748dde75b92b
rylee12 <ryanlee7x@gmail.com>
2019-05-10 18:52:16 -0700

    DragObject Update #1
    It Kinda works

--- Seeds of Love/Assets/Script/MiscSystems/DragObject.cs
+++ Seeds of Love/Assets/Script/MiscSystems/DragObject.cs
@@ -4,27 +4,55 @@ using UnityEngine;
 
 public class DragObject : MonoBehaviour {
 
-    private Vector3 x;
-    private Vector3 y;
-    private Vector3 z;
+    //protected Rigidbody2D r2bd2;
+    private Vector3 force;
+    private Vector3 a;
+    private Vector3 b;
+    private Vector3 c;
+    private Vector3 j;
+    private float speedMult = 6;
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
+        //gameObject.GetComponent<Rigidbody2D>().velocity = force;
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
 	}
 
     void OnMouseDown()
     {
-        x = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-        y = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, x.z));
+        a = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        b = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, a.z));
     }
     void OnMouseDrag()
     {
-        Vector3 a = new Vector3(Input.mousePosition.x, Input.mousePosition.y, x.z);
-        Vector3 b = Camera.main.ScreenToWorldPoint(a) + y;
-        transform.position = b;
+        Vector3 curPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, a.z);
+        float dist = Vector3.Distance(curPos, gameObject.transform.position);
+        Debug.Log(dist);
+        /*if (dist == 0)
+        {
+            force = Vector3.zero;
+        }
+        else if (dist > 0)
+        {
+            force = (Camera.main.ScreenToWorldPoint(curPos) - b) * speedMult;
+        }
+        else
+        {
+            force = (Camera.main.ScreenToWorldPoint(curPos) + b) * speedMult;
+        }*/
+        force = Camera.main.ScreenToWorldPoint(curPos) + b;
+        //gameObject.GetComponent<Rigidbody2D>().velocity = force * speedMult;
+        transform.position = force;
+        //gameObject.GetComponent<Rigidbody2D>().velocity = force * speedMult;
     }
 
+    void OnMouseUp()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = force * speedMult; 
+    }
     /*
      *  x = Camera.main.WorldToScreenPoint(gameObject.transform.position);
      *  y = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, x.z));

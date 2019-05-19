1200823e7aa84ea607f9fb2b41dd114916535367
Justin Morales <magagod2@gmail.com>
2019-05-18 17:24:55 -0700

    Edited Script
    Scroll script was added with scroll speed

--- Seeds of Love/Assets/Script/MiscSystems/Credits/CreditsScroll.cs
+++ Seeds of Love/Assets/Script/MiscSystems/Credits/CreditsScroll.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreditsScroll : MonoBehaviour {
 
+    public float ScrollSpeed;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Vector2 pos = gameObject.transform.position;
+        gameObject.transform.position = new Vector2(pos.x, pos.y+ScrollSpeed);
+    }
 }
